659cc28ebf1579aa2209604db4bd983ab9b95daf
JibiiBabu <118193311+JibiiBabu@users.noreply.github.com>
2023-02-20 23:09:47 +0530

    Add files via upload

---
+++ graphics/__init__.py
@@ -0,0 +1,16 @@
+import Grapgics.rectangle as re
+import Grapgics.circle as c
+import Grapgics.trdgraphics.cuboid as cb
+import Grapgics.trdgraphics.sphere as s
+print("Rectangle")
+print("Area:", re.area(5, 6))
+print("Perimeter:", re.perimeter(5, 6))
+print("Circle")
+print("Area:", c.area(4))
+print("Perimeter:", c.perimeter(4))
+print("Cuboid")
+print("Area:", cb.area(4, 5, 6))
+print("Perimeter:", cb.perimeter(4, 5, 6))
+print("Shere")
+print("Area:", s.area(5))
+print("Perimeter:", s.perimeter(5))
